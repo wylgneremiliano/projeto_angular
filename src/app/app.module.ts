4224cdd763e66e59fe3ae34659abdac2f9c127f5
Wylgner Emiliano <wylgneremiliano@gmail.com>
2022-03-28 10:11:56 -0300

    feat: hello World

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TesteComponent
   ],
   imports: [
     BrowserModule
